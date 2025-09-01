58c931157ba3f35c307921a3a9b1758d3f317ce8
ulcheyev <ulcheyev@fel.cvut.cz>
2025-09-01 08:36:59 +0200

    test prettier precommit

--- src/app/Test.tsx
+++ src/app/Test.tsx
@@ -1,4 +1,5 @@
-import   React    from  "react"
+import
+    React    from  "react"
 
 
 export default function TestPrettier ( )    {
@@ -6,10 +7,10 @@ export default function TestPrettier ( )    {
     const [ count, setCount] = React.useState(0)
     return (
         <div   style={ { padding : "2rem" } }>
-            <h2>Test Prettier</h2>
-            <button
-                onClick = {()=> setCount( c => c+1 ) }    >
-                Clicked { count } times
+<h2>Test Prettier</h2>
+<button
+    onClick = {()=> setCount( c => c+1 ) }    >
+Clicked { count } times
             </button>
         </div>
     )
